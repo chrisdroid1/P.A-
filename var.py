1888be68cd76695c76f95ef37b277f70547f718f
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-05-07 21:46:25 +0100

    Update var.py

--- var.py
+++ var.py
@@ -1,10 +1,24 @@
-import os
-
-ENV = bool(os.environ.get("ENV", False))
-if ENV:
-    from heroku_config import Var as config
-else:
-    from localconfig import config
+from main_startup.core.decorators import friday_on_cmd
+from main_startup.helper_func.basic_helpers import edit_or_reply, get_text, edit_or_send_as_file
+import calendar
+from datetime import datetime
 
 
-Var = config
+@borg.on(admin_cmd(outgoing=True, pattern="utime(?: |$)(.*)(?<![0-9])(?: |$)([0-9]+)?"))
+@bot.on(
+    sudo_cmd(
+        outgoing=True,
+        pattern="utime(?: |$)(.*)(?<![0-9])(?: |$)([0-9]+)?",
+        allow_sudo=True,
+     )
+ )
+async def _d(client, message):
+    year_ = datetime.now().year
+    date_ = datetime.now().day
+    month_ = datetime.now().month
+    mydate = datetime.now()
+    da = mydate.strftime("Date : %d \nMonth : %B \nYear : %Y")
+    dt = mydate.strftime("Hour : %H \nMinute : %M")
+    cal_ = calendar.month(year_, month_)
+    f_d = f"<code>{cal_}\n{da} \n\n{dt}</code>"
+    await edit_or_reply(message, f_d, parse_mode="html")
